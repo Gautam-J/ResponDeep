11aa0c4fcee9ffb42398b8638e8453bdd5d7365d
Gautam J <gautam.jayapal@gmail.com>
2020-11-11 19:47:38 +0530

    update utils

--- utils.py
+++ utils.py
@@ -1,5 +1,37 @@
 import os
+import time
+import pathlib
+
+
+def createBaseDir():
+    baseDir = f'models/model_{int(time.time())}'
+
+    if not os.path.exists(baseDir):
+        os.makedirs(baseDir)
+
+    return baseDir
 
 
 def getNumberOfPNGInDirectory(directory):
     return len([i for i in os.listdir(directory) if i.endswith('.png')])
+
+
+def getNumberOfClasses(path_to_train_dir):
+    return len(list(os.listdir(path_to_train_dir)))
+
+
+def getNumberOfTrainingImages(path_to_train_dir):
+    return len(list(pathlib.Path(path_to_train_dir).glob('*/*.png')))
+
+
+def getClassWeights(path_to_train_dir):
+    nClasses = getNumberOfClasses(path_to_train_dir)
+    numberOfTrainingImages = getNumberOfTrainingImages(path_to_train_dir)
+    classList = sorted(os.listdir(path_to_train_dir))
+
+    classWeights = {}
+    for sno, className in enumerate(classList):
+        bincount = len(list(os.listdir(os.path.join(path_to_train_dir, className))))
+        classWeights[sno] = numberOfTrainingImages / (nClasses * bincount)
+
+    return classWeights
