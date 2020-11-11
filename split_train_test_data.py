fe2082f9f6f154a9146a6dae2410f1b19a007647
Gautam J <gautam.jayapal@gmail.com>
2020-11-11 19:32:35 +0530

    update split data, change folder arch

--- split_train_test_data.py
+++ split_train_test_data.py
@@ -17,37 +17,47 @@ def getArgs():
 args = getArgs()
 testSize = args.size
 
+trainDirectory = os.path.join('data', 'train')
+validationDirectory = os.path.join('data', 'validation')
+
+if not os.path.exists(trainDirectory):
+    os.makedirs(trainDirectory)
+
+if not os.path.exists(validationDirectory):
+    os.makedirs(validationDirectory)
+
 for labelName in os.listdir('data'):
+    if labelName in ['train', 'validation']:
+        continue
+
     print(f'[INFO] Parsing label {labelName}')
     trainCount = 0
     testCount = 0
 
     labelDirectory = os.path.join('data', labelName)
-    trainDirectory = os.path.join(labelDirectory, 'train')
-    testDirectory = os.path.join(labelDirectory, 'test')
-
-    if not os.path.exists(trainDirectory):
-        os.makedirs(trainDirectory)
-
-    if not os.path.exists(testDirectory):
-        os.makedirs(testDirectory)
-
     totalNumberOfFiles = getNumberOfPNGInDirectory(labelDirectory)
     testFileMax = floor(testSize * totalNumberOfFiles)
 
-    for fileName in os.listdir(labelDirectory):
-        if not fileName.endswith('.png'):
-            continue
+    labelTrainDirectory = os.path.join(trainDirectory, labelName)
+    labelValidationDirectory = os.path.join(validationDirectory, labelName)
 
+    if not os.path.exists(labelTrainDirectory):
+        os.makedirs(labelTrainDirectory)
+
+    if not os.path.exists(labelValidationDirectory):
+        os.makedirs(labelValidationDirectory)
+
+    for fileName in os.listdir(labelDirectory):
         originalPathToFile = os.path.join(labelDirectory, fileName)
 
         if int(fileName.split('.')[0]) < testFileMax:
-            newPathToFile = os.path.join(testDirectory, fileName)
+            newPathToFile = os.path.join(labelValidationDirectory, fileName)
             testCount += 1
         else:
-            newPathToFile = os.path.join(trainDirectory, fileName)
+            newPathToFile = os.path.join(labelTrainDirectory, fileName)
             trainCount += 1
 
         os.rename(originalPathToFile, newPathToFile)
 
+    os.rmdir(labelDirectory)
     print(f"[DEBUG] {trainCount=} {testCount=}\n")
